8493af2b36c8f08ec8c34d9190aead9c9fa8ba1f
18107 <18107@users.noreply.github.com>
2024-06-10 10:09:03 +0930

    Add files via upload

---
+++ AtmosphereFix/LifeSupportModulePatch.cs
@@ -0,0 +1,38 @@
+﻿using CG.Ship.Modules;
+using Gameplay.Atmosphere;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmosphereFix
+{
+    [HarmonyPatch(typeof(LifeSupportModule))]
+    internal class LifeSupportModulePatch
+    {
+        private static readonly MethodInfo GetAtmosphereValuesMethod = AccessTools.Method(typeof(Atmosphere), "GetAtmosphereValues");
+        private static readonly MethodInfo SetAtmosphereValuesMethod = AccessTools.Method(typeof(Atmosphere), "SetAtmosphereValues");
+
+        [HarmonyPrefix]
+        [HarmonyPatch("ApplyPressureIncrease")]
+        static bool ApplyPressureIncrease()
+        {
+            return false; //Don't run original method
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("ApplyOxygenIncrease")]
+        static void ApplyOxygenIncrease(Atmosphere ____atmosphere)
+        {
+            foreach(Room room in ____atmosphere.Atmospheres.keys)
+            {
+                AtmosphereValues atmosphereValues = (AtmosphereValues)GetAtmosphereValuesMethod.Invoke(____atmosphere, new object[] { room });
+                atmosphereValues.Pressure = atmosphereValues.Oxygen;
+                SetAtmosphereValuesMethod.Invoke(____atmosphere, new object[] { room, atmosphereValues });
+            }
+        }
+    }
+}
---
+++ AtmosphereFix/VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace AtmosphereFix
+{
+    public class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.Host;
+
+        public override string Author => "18107";
+
+        public override string Description => "Synchronises O2 and pressure";
+    }
+}
